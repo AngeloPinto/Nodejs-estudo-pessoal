9e68824243b8d13c21e62482aeb260c080ac27a0
Angelo Rafael Peres Pinto <angelo.pinto@adm.uninorte.com.br>
2019-10-11 10:23:26 -0400

    servidor node respondendo html

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 var http = require('http');
 
 function handle(request, response) {
-    response.end('Hello from http module');
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+
+    response.write('<!DOCTYP "html">');
+    response.write('<html>');
+    response.write('<head>');
+    response.write('<meta charset="utf-8">');
+    response.write('<title>');
+    response.write('Server Node');
+    response.write('</title>');
+    response.write('</head>');
+    response.write('<body>');
+    response.write('<h1>Este é um servidor node</h1>');
+    response.write('</body>');
+    response.write('</html>');
+
+    response.end();
 }
 
 var server = http.createServer(handle);
