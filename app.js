2b7dec4ffed672f41d8fb8a801bdb86236583e98
Angelo Rafael Peres Pinto <angelo.pinto@adm.uninorte.com.br>
2019-10-11 15:43:41 -0400

    trabalhando com eventos no Node

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 var http   = require('http');
 var handle = require('./handle');
+var events = require('events');
+var emiter = new events.EventEmitter();
+
+
+
+// emiter.on('say12', say);
+
+// function say() {
+//     console.log('Im say');
+// }
+
+// emiter.emit('say12');
 
 var server = http.createServer(handle);
 
--- handle.js
+++ handle.js
@@ -1,6 +1,18 @@
+var querystring = require('querystring');
+
 function handle(request, response) {
     response.writeHead(200, {
         'Content-Type': 'text/html'
+    });
+
+    var body = '';
+    request.on('data', function(valor){
+        body += valor.toString();
+    });
+    
+    request.on('end', function() {
+        var decode = querystring.parse(body);
+        console.log(decode);
     });
 
     response.write('<!DOCTYP "html">');
